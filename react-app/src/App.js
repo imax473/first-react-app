6e09509e367c033fd8f1c88b66179d2a0112cb7b
imax473 <imax473@gmail.com>
2022-08-19 15:33:03 +0300

    Change App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>Learn Github</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, quia!</p>
     </div>
   );
 }
